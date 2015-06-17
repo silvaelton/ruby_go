745b722ca93a43c8c94410b791f07536dbf0db94
Elton Silva <elton.chrls@gmail.com>
2015-06-17 16:09:53 -0300

    alterção qualquer

--- core/array/basic.rb
+++ core/array/basic.rb
@@ -1,5 +1,3 @@
-# create new array
-
 # A new array can be created by using the literal constructor [].
 # Arrays can contain different types of objects.
 # For example, the array below contains an Integer, a String and a Float:
@@ -9,9 +7,9 @@ array = [1, 'two', 3.0] #=> [1, 'two', 3.0]
 # An array can also be created by explicitly calling ::new with zero,
 # one (the initial size of the Array) or two arguments (the initial size and a default object).
 
-array = Array.new #=> []
-array = Array.new(2) #=> [nil, nil]
-array = Array.new(2, true) #=> [true, true]
+array = Array.new           #=> []
+array = Array.new(2)        #=> [nil, nil]
+array = Array.new(2, true)  #=> [true, true]
 
 # To create an array with separate objects a block can be passed instead.
 # This method is safe to use with mutable objects such as hashes, strings or other arrays:
@@ -29,5 +27,8 @@ array = Array.new({:a => "a", :b => "b"}) #=> [[:a, "a"], [:b, "b"]]
 # Negative indices start counting from the end, with -1 being the last element.
 
 array = [1,2,3,4,5,6,7,8]
-array[-1] #=> 8
-array[0] #=> 1
+
+array[-1]   #=> 8
+array[0]    #=> 1
+array.first #=> 1
+array.last  #=> 8
